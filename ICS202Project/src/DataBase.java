819a8c86399506218235303d85f7357efca496fb
Yousef Mahmoud <mryoyo133@gmail.com>
2024-12-13 20:33:50 +0300

    intial DataBase class

---
+++ ICS202Project/src/DataBase.java
@@ -0,0 +1,29 @@
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class DataBase {
+    Hashtable<String, ArrayList<Integer>> levelTable;
+    AVLTree<Integer> idTree;
+
+    public DataBase(){
+        levelTable = new Hashtable<String, ArrayList<Integer>>();
+        idTree = new AVLTree<Integer>();
+    }
+
+    public boolean addStudent(Student student){
+        return true;
+    }
+
+    public boolean deleteStudent(Student student){
+        return true;
+    }
+
+    public ArrayList<Student> getStudentsByFirstName(String firstName){}
+    public ArrayList<Student> getStudentsByLastName(String lastName){}
+    public ArrayList<Student> getStudentsByLevel(String level){}
+    public Student getStudentById(int id){}
+
+
+
+}
--- ICS202Project/src/Main.java
+++ ICS202Project/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Student s = new Student(202278640, "Yousef", "Abdelaziz", "09/01/2005", "SO");
+        Student s = new Student(202278640, "Abdelaziz", "Yousef", "09/01/2005", "SO");
         System.out.println(s);
-
     }
 }
