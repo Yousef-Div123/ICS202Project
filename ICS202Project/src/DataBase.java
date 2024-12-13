c89085a117edf3ad3e5610303c758f19e5e3b9ae
M3ZH2 <mtsat2005@gmail.com>
2024-12-13 21:12:08 +0300

    ِ

--- ICS202Project/src/DataBase.java
+++ ICS202Project/src/DataBase.java
@@ -4,25 +4,43 @@ import java.util.Hashtable;
 
 public class DataBase {
     Hashtable<String, ArrayList<Student>> levelTable;
-    AVLTree<Student> idTree;
+    AVLTree<Integer> idTree;
 
     public DataBase(){
         levelTable = new Hashtable<String, ArrayList<Integer>>();
-        idTree = new AVLTree<Student>();
+        idTree = new AVLTree<Integer>();
     }
 
-    public boolean addStudent(Student student){{
+    public boolean addStudent(Student student){
+        idTree.insert(student.getId());
+        if(levelTable.containsKey(student.getUniversityLevel())){
+            levelTable.get(student.getUniversityLevel()).add(student.getId());
+        }else{
+            ArrayList<Integer> newArray = new ArrayList<>();
+            newArray.add(student.getId());
+            levelTable.put(student.getUniversityLevel(), newArray);
+        }
         return true;
     }
 
     public boolean deleteStudent(Student student){
+        idTree.deleteAVL(student.getId());
         return true;
     }
 
-    public ArrayList<Student> getStudentsByFirstName(String firstName){}
-    public ArrayList<Student> getStudentsByLastName(String lastName){}
-    public ArrayList<Student> getStudentsByLevel(String level){}
-    public Student getStudentById(int id){}
+    public ArrayList<Student> getStudentsByFirstName(String firstName){
+        return null;
+    }
+    public ArrayList<Student> getStudentsByLastName(String lastName){
+        return null;
+    }
+    public ArrayList<Student> getStudentsByLevel(String level){
+        return null;
+    }
+    public Student getStudentById(int id){
+        return null;
+    }
+
 
 
 }
--- ICS202Project/src/Main.java
+++ ICS202Project/src/Main.java
@@ -1,6 +1,38 @@
-public class Main {
-    public static void main(String[] args){
-        Student s = new Student(202278640, "Yousef", "Abdelaziz", "09/01/2005", "SO");
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Main {
+    public Main() throws FileNotFoundException {
+    }
+
+    public static void main(String[] args){
+        //Student s = new Student(202278640, "Yousef", "Abdelaziz", "09/01/2005", "SO");
+        //System.out.println(s);
+        loadData();
+    }
+
+    public static void loadData(){
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("students-details (1).csv"))) {
+            String line;
+            String firstline= br.readLine();
+            DataBase db = new DataBase();
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                Student newStudent = new Student(Integer.parseInt(values[0]), values[1], values[1], values[3], values[4]);
+                System.out.println(newStudent);
+                db.addStudent(newStudent);
+                records.add(Arrays.asList(values));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
