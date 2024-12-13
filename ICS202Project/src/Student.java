ffab58649616cbac158972025c7d0ef9887ce2c9
Yousef Mahmoud <mryoyo133@gmail.com>
2024-12-13 20:02:21 +0300

    student class

---
+++ ICS202Project/src/Student.java
@@ -0,0 +1,55 @@
+public class Student {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String dateOfBirth;
+    private String universityLevel;
+
+    public Student(int id, String lastName, String firstName, String dateOfBirth, String universityLevel) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+        this.universityLevel = universityLevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUniversityLevel() {
+        return universityLevel;
+    }
+
+    public void setUniversityLevel(String universityLevel) {
+        this.universityLevel = universityLevel;
+    }
+}
